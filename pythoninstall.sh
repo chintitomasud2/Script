844066aa750bc318997f2399876dfa307bc68f00
chintitomasud2 <foraiubccna@gmail.com>
2017-10-11 13:36:39 +0600

    Create pythoninstall.sh

---
+++ pythoninstall.sh
@@ -0,0 +1,7 @@
+wget -nc  https://www.python.org/ftp/python/3.6.3/Python-3.6.3.tar.xz
+echo "Downloading finished"
+mv Python-3.6.3.tar.xz ~/Downloads
+cd ~/Downloads
+echo "Extracking"
+tar -xJf Python-3.6.3.tar.xz
+echo "Extracking Done"
