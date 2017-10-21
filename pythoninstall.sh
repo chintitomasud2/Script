78ddedc2a1916fbc2c44e3aa9e164d2d36f5c6f5
chintitomasud2 <foraiubccna@gmail.com>
2017-10-22 00:09:02 +0600

    Update pythoninstall.sh

--- pythoninstall.sh
+++ pythoninstall.sh
@@ -1,5 +1,5 @@
 cd  ~/Downloads
-sudo yum groupinstall "Development Tools"
+sudo yum -y groupinstall "Development Tools"
 sudo yum -y install yum-utils yum-builddep python zlib-devel gcc
 wget -nc  https://www.python.org/ftp/python/3.6.3/Python-3.6.3.tar.xz
 echo "Downloading finished"
@@ -10,6 +10,6 @@ tar -xJf Python-3.6.3.tar.xz
 echo "Extracking Done"
 cd Python-3.6.3
 ./configure
-sudo make
-sudo make altinstall
+sudo make 
+sudo make altinstall 
 which python3 && python3 -V
