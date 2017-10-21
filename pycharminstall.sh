1cec0a41ce26abf02a911175ed57420a68d9a129
chintitomasud2 <foraiubccna@gmail.com>
2017-10-22 01:56:49 +0600

    Update pycharminstall.sh

--- pycharminstall.sh
+++ pycharminstall.sh
@@ -1,1 +1,6 @@
 wget https://download-cf.jetbrains.com/python/pycharm-community-2017.2.3.tar.gz
+tar -xzvf pycharm-community-2017.2.3.tar.gz 
+cd pycharm-community-2017.2.3/
+cd bin/
+bash pycharm.sh
+
