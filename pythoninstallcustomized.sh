ea479a9ab0df160a73cd1dc2423217243a317dfc
chintitomasud2 <foraiubccna@gmail.com>
2017-10-29 15:21:52 +0600

    Create pythoninstallcustomized.sh

---
+++ pythoninstallcustomized.sh
@@ -0,0 +1,16 @@
+cd  ~/Downloads
+sudo yum -y groupinstall "Development Tools"
+sudo yum -y install yum-utils yum-builddep python zlib-devel gcc
+wget -nc  https://www.python.org/ftp/python/3.6.3/Python-3.6.3.tar.xz
+echo -e "\033[5;32;40m Succesefully Downloaded \033[0m"
+#echo "Downloading finished"
+sleep(2000)
+
+echo -e "\033[5;34;40m Extracing \033[0m"
+tar -xJf Python-3.6.3.tar.xz
+echo -e "\033[5;32;40m Succesefully Extracted \033[0m"
+cd Python-3.6.3
+sudo ./configure
+sudo make 
+sudo make altinstall 
+which python3 && python3 -V
